d050a01977e7dfdaa1b4342b3ae340ce8177133f
Kings <ebucekings62@gmail.com>
2025-01-29 21:49:25 -1200

    chatbot

---
+++ React/chatbot/src/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ChatInput() {
+  return (
+    <>
+     <input placeholder='Send a messesge to Chatbot' size='35' />
+     <button>Send</button>
+    </>
+  )
+}
+
+export default ChatInput;
